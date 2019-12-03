9aacb68c75a4e5b870dfe69f9cb24f3566451f9d
sebv <sebv@sv.lan>
2019-12-03 14:20:20 +0100

    recupération requete axios get

--- src/components/Search.js
+++ src/components/Search.js
@@ -16,7 +16,7 @@ export default function Search() {
           </button>
         </form>
       </div>
-      <div class="svg">
+      <div className="svg">
         <svg
           id="Layer_1"
           data-name="Layer 1"
@@ -26,7 +26,7 @@ export default function Search() {
           preserveAspectRatio="xMaxYMax meet"
         >
           <path
-            class="cls"
+            className="cls"
             d="M0,601a1849.2,1849.2,0,0,1,370-47c246.77-6.15,360,41.14,613,38,95.54-1.19,226.52-9.76,383-42q-.26,108.75-.5,217.5H-.5Z"
             transform="translate(0.5 -550)"
           ></path>
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -12,7 +12,7 @@ import CardRestaurant from "../components/CardRestaurant";
 export default function Home() {
   // Création des états
   const [isLoading, setIsLoading] = useState(true);
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState("");
 
   // useEffect permet de charger les données uniquement à la création du composant
   useEffect(() => {
@@ -21,8 +21,9 @@ export default function Home() {
         const response = await axios.get(
           "https://res.cloudinary.com/lereacteur-apollo/raw/upload/v1575242111/10w-full-stack/Scraping/restaurants.json"
         );
-        console.log(response.data);
-        console.log(response.data.name);
+        console.log(response.data); // [{...}]
+        console.log(response.data[0].name); // Veganie
+        console.log(response.data[0].address); // 96 rue Quincampoix, Paris, France, 75003
 
         setProducts(response.data);
         setIsLoading(false);
@@ -46,10 +47,26 @@ export default function Home() {
             </p>
           </div>
           <div className="card-list center">
-            <CardRestaurant />
-            <CardRestaurant />
-            <CardRestaurant />
-            <CardRestaurant />
+            <CardRestaurant
+              picture={products[0].pictures[0]}
+              name={products[0].name}
+              description={products[0].description}
+            />
+            <CardRestaurant
+              picture={products[1].pictures[0]}
+              name={products[1].name}
+              description={products[1].description}
+            />
+            <CardRestaurant
+              picture={products[2].pictures[0]}
+              name={products[2].name}
+              description={products[2].description}
+            />
+            <CardRestaurant
+              picture={products[3].pictures[0]}
+              name={products[3].name}
+              description={products[3].description}
+            />
           </div>
         </section>
       )}
