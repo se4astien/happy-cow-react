65b55a6deed526cbf782ed1ea7ec8e7ffa5658e6
sebv <sebv@sv.home>
2019-12-14 17:24:29 +0100

    gallery en cours

--- src/components/CardProductDetailsGallery.js
+++ src/components/CardProductDetailsGallery.js
@@ -1,17 +1,13 @@
 import React from "react";
 
 const CardProductDetailsGallery = ({ pictures, name }) => {
-  const tabPicture = [];
-  for (let i = 0; i < pictures.length; i++) {
-    tabPicture.push(pictures[i]);
-  }
-  console.log(tabPicture);
-
   return (
     <>
       <div className="listing-images flex">
         <div>
-          <img src={tabPicture} alt={name} />
+          {pictures.map((picture, index) => {
+            return <img key={index} src={picture} alt={name} />;
+          })}
         </div>
       </div>
     </>
